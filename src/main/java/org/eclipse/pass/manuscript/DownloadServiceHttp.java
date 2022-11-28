558890272e2562fc6b65181f7259f0405dd2f4ad
maggieolaya <molaya1@jhu.edu>
2022-11-28 08:26:20 -0500

    further design outline

---
+++ src/main/java/org/eclipse/pass/manuscript/DownloadServiceHttp.java
@@ -0,0 +1,14 @@
+package org.eclipse.pass.manuscript;
+/*
+ * 
+ * 
+ * @author Maggie Olaya
+ */
+
+public class DownloadServiceHttp {
+    public void downloadServiceHandler(DownloadService downloadService){
+
+        
+        //calls download_service.Download, passes doi and uri
+    }
+}
---
+++ src/main/java/org/eclipse/pass/manuscript/Manuscript.java
@@ -0,0 +1,31 @@
+package org.eclipse.pass.manuscript;
+
+/*
+ * Contains the info for the manuscripts that
+ * will be found during DOI lookup
+ * 
+ * @author Maggie Olaya
+ */
+public class Manuscript{
+    private String location; //location URI of manuscript
+    private String repoInstitution; //readable label for the repository where the article can be found
+    private String type; //the MIME type of manuscript file
+    private String source; //the API where we found the file
+    private String name; //the file name
+
+    public void manuscript(String location, String repoInstitution, String type, String source, String name){
+        this.location = location;
+        this.repoInstitution= repoInstitution;
+        this.type = type;
+        this.source = source;
+        this.name = name;
+    }
+
+    public String getLocation(){return location;}
+    public String getRepoInstitution(){return repoInstitution;}
+    public String getType(){return type;}
+    public String getSource(){return source;}
+    public String getName(){return name;}
+
+    //need setters?
+}
