6cd629200dd3617850b2730082a57110cc0d9cb0
maggieolaya <molaya1@jhu.edu>
2022-12-11 19:01:48 -0500

    fixed checksyle errors

--- src/main/java/org/eclipse/pass/manuscript/Manuscript.java
+++ src/main/java/org/eclipse/pass/manuscript/Manuscript.java
@@ -6,26 +6,44 @@ package org.eclipse.pass.manuscript;
  * 
  * @author Maggie Olaya
  */
-public class Manuscript{
-    private String location; //location URI of manuscript
-    private String repoInstitution; //readable label for the repository where the article can be found
-    private String type; //the MIME type of manuscript file
-    private String source; //the API where we found the file
-    private String name; //the file name
+public class Manuscript {
+  private String location; //location URI of manuscript
+  private String repoInstitution; //readable label for the repository where the article can be found
+  private String type; //the MIME type of manuscript file
+  private String source; //the API where we found the file
+  private String name; //the file name
 
-    public void manuscript(String location, String repoInstitution, String type, String source, String name){
-        this.location = location;
-        this.repoInstitution= repoInstitution;
-        this.type = type;
-        this.source = source;
-        this.name = name;
-    }
+  /**
+   * Manuscript creation.
+   * 
+   */
+  public void manuscript(String location, String repo, String type, String source, String name) {
+    this.location = location;
+    repo = repoInstitution;
+    this.type = type;
+    this.source = source;
+    this.name = name;
+  }
 
-    public String getLocation(){return location;}
-    public String getRepoInstitution(){return repoInstitution;}
-    public String getType(){return type;}
-    public String getSource(){return source;}
-    public String getName(){return name;}
+  public String getLocation() {
+    return location;
+  }
 
-    //need setters?
+  public String getRepoInstitution() {
+    return repoInstitution;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public String getSource() {
+    return source;
+  }
+  
+  public String getName() {
+    return name;
+  }
+
+  //need setters?
 }
