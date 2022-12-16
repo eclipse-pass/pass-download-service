525cc8c406572e58465d0c46daac9fea5119c460
maggieolaya <molaya1@jhu.edu>
2022-12-16 18:24:56 -0500

    checkstyle

--- src/main/java/org/eclipse/pass/manuscript/LookupServlet.java
+++ src/main/java/org/eclipse/pass/manuscript/LookupServlet.java
@@ -13,9 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
-
 @WebServlet("/lookupservice")
 public class LookupServlet extends HttpServlet {
 
