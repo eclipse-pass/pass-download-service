a4b4888d467aad4bc7fbbec46129bff4565b1664
maggieolaya <molaya1@jhu.edu>
2022-12-16 19:50:28 -0500

    changed indentation

--- src/main/java/org/eclipse/pass/manuscript/LookupServlet.java
+++ src/main/java/org/eclipse/pass/manuscript/LookupServlet.java
@@ -1,42 +1,42 @@
-	package org.eclipse.pass.manuscript;
+package org.eclipse.pass.manuscript;
 
-	/*
-	* Starts the web service with the user
-	* 
-	* @author Maggie Olaya
-	*/
+/*
+ * Starts the web service with the user
+ * 
+ * @author Maggie Olaya
+ */
 
-	import java.io.IOException;
-	import javax.servlet.ServletException;
-	import javax.servlet.annotation.WebServlet;
-	import javax.servlet.http.HttpServlet;
-	import javax.servlet.http.HttpServletRequest;
-	import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-	@WebServlet("/lookupservice")
-	public class LookupServlet extends HttpServlet {
+@WebServlet("/lookupservice")
+public class LookupServlet extends HttpServlet {
 
-		/**
-		* Lookup service.
-		*/
-		public void lookupService() {
-			//sets up http, fedora
-			Unpaywall unpaywall = new Unpaywall();
-			LookupServiceHttp lookupService = new LookupServiceHttp();
-		}
+  /**
+  * Lookup service.
+  */
+  public void lookupService() {
+    //sets up http, fedora
+    Unpaywall unpaywall = new Unpaywall();
+    LookupServiceHttp lookupService = new LookupServiceHttp();
+  }
 
-		@Override
-		protected void doGet(HttpServletRequest request, HttpServletResponse response)
-						throws ServletException, IOException {
-				//TODO: implement
-						
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+      //TODO: implement
+          
 
-		}
+  }
 
-		@Override
-		protected void doPost(HttpServletRequest request, HttpServletResponse response)
-						throws ServletException, IOException {
-				//TODO: implement
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+      //TODO: implement
 
-		}
-	}
+  }
+}
